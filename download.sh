b81831b3c954e007f01094028088694d48a97ce1
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2025-03-18 23:24:34 +0100

    feat: wip download

--- download.sh
+++ download.sh
@@ -1,3 +1,23 @@
+apt install -y go make
+
+git clone https://github.com/containernetworking/plugins
+cd plugins
+bash build_linux.sh
+mkdir -p /opt/cni/bin
+sudo cp bin/* /opt/cni/bin/
+cd ..
+git clone https://github.com/awslabs/tc-redirect-tap
+cd tc-redirect-tap
+make
+sudo cp tc-redirect-tap /opt/cni/bin/
+cd ..
+
+wget https://github.com/firecracker-microvm/firecracker/releases/download/v1.11.0/firecracker-v1.11.0-x86_64.tgz
+tar -xzf firecracker-v1.11.0-x86_64.tgz
+cp release-v1.11.0-x86_64/firecracker-v1.11.0-x86_64 /usr/bin/firecracker
+
+rm -rf release-v1.11.0-x86_64 firecracker-v1.11.0-x86_64.tgz
+
 ARCH="$(uname -m)"
 
 latest=$(wget "http://spec.ccfc.min.s3.amazonaws.com/?prefix=firecracker-ci/v1.11/$ARCH/vmlinux-5.10&list-type=2" -O - 2>/dev/null | grep -oP "(?<=<Key>)(firecracker-ci/v1.11/$ARCH/vmlinux-5\.10\.[0-9]{1,3})(?=</Key>)")
@@ -23,3 +43,6 @@ truncate -s 400M ubuntu-24.04.ext4
 sudo mkfs.ext4 -d squashfs-root -F ubuntu-24.04.ext4
 
 rm -rf squashfs-root ubuntu-24.04.squashfs.upstream
+
+
+go mod tidy
